46f87b6d72233999e1b0ab54b4080dc35d52d2cb
Takagi Tasuku <tasuren@outlook.jp>
2023-05-27 11:40:15 +0900

    今となっては過去のものを削除。

--- includes/assets/articles/page.js
+++
@@ -1,9 +0,0 @@
-// tasuren.xyz Articles - Page
-
-import { setHashTagList } from "./common.js";
-
-
-// 全てのハッシュタグを表示する。
-window.addEventListener("load", (_) => setHashTagList(
-  document.getElementById("tags").getAttribute("data-tags").split(",")
-));
