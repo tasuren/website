daa570ae688e5f31ac1e11fcd77922440e2b8327
Takagi Tasuku <tasuren@outlook.jp>
2023-05-27 17:22:31 +0900

    恐らく完成。

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,34 @@ import { defineConfig } from 'astro/config';
 
 import { setDefaultOptions } from 'date-fns';
 import jaLocale from "date-fns/locale/ja";
+//import prefetch from "@astrojs/prefetch";
 
 
 const DEFAULT_LAYOUT = '/src/layouts/Main.astro';
-
 function setDefaultLayout() {
   return function (_, file) {
-    const { frontmatter } = file.data.astro;
+    const {
+      frontmatter
+    } = file.data.astro;
     if (!frontmatter.layout) frontmatter.layout = DEFAULT_LAYOUT;
   };
-};
+}
+;
 
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [setDefaultLayout]
   },
   experimental: {
     assets: true
-  }
+  },
+  //integrations: [prefetch()]
 });
 
 
 // デフォルトのdate-fnsの言語設定を日本語にする。 
-setDefaultOptions({locale: jaLocale});
+setDefaultOptions({
+  locale: jaLocale
+});
