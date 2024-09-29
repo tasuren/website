aab93578a157201d699f8c0565a888a07867d909
Takagi Tasuku <tasuren@outlook.jp>
2024-09-29 15:55:28 +0900

    importの順番を調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
