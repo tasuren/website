84ff800dc5938b7d77a3420d89ad7fa3b2f7d0ff
Takagi Tasuku <tasuren@outlook.jp>
2026-02-24 09:05:25 +0900

    削除済みの依存関係を使っているのを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://tasuren.jp",
     prefetch: true,
-    integrations: [sitemap(), mdx(), icon()],
+    integrations: [sitemap(), mdx()],
     vite: {
         plugins: [tailwindcss()],
     },
